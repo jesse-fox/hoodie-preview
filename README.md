9b29d67949d04fc91838cd56f424f1284c13aa44
J Fox <janevfox@protonmail.com>
2017-03-08 23:20:53 -0700

    Fixed imagick image display, comments

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
+
 # hoodie-preview
+
 Quick script to help show what different color combinations would look like
 
-Running
----
+
+
+## Running
 
 Requires a webserver like nginx with php
 You must install image magick, probably
 ` sudo apt install php-imagick
 May have to reload php and server, eg
-` sudo server php7.0-fpm restart
+` sudo service php7.0-fpm restart
+Index file has a base url in the script you might have to update
--- image.php
+++ image.php
@@ -6,22 +6,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
+// Expecting path such as http://localhost/hoodie-preview/image.php?fe8101/3c3c3c
 $_SERVER['REQUEST_URI_PATH'] = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$segments = explode('/', $_SERVER['REQUEST_URI_PATH']);
+$segments = explode('/', $_SERVER['QUERY_STRING']);
 
-//print_r($segments);
 
 $background = "#000";
 
-$main_color = "#" . $segments[1];
-$second_color = "#" . $segments[2];
+$main_color = "#" . $segments[0];
+$second_color = "#" . $segments[1];
 
+//Random hex colors
 //$main_color = "rgb(".mt_rand(0,255).",".mt_rand(0,255).",".mt_rand(0,255).")";
 //$second_color = "rgb(".mt_rand(0,255).",".mt_rand(0,255).",".mt_rand(0,255).")";
 
 
-
+// Set up for putting our image together
 $sleeves_img = new Imagick('img/sleeves.jpg');
 $body_img = new Imagick('img/body.jpg');
 $ghosts = new Imagick('img/over.png');
@@ -29,39 +29,37 @@ $ghosts = new Imagick('img/over.png');
 $height = $sleeves_img->getImageHeight();
 $width = $sleeves_img->getImageWidth();
 
+$canvas = new Imagick();
+$canvas->newImage($width, $height, $background, "jpg");
 
 
+// Composite the images with colors, and combine them
 try{
 
 
-	$canvas = new Imagick();
-	$canvas->newImage($width, $height, $background, "jpg");
-
-
+	// Sleeve image - Generate and color
 	$sleeve_color = new Imagick();
 	$sleeve_color->newPseudoImage($width,$height,"canvas:" . $second_color);
-
 	$sleeve_color->compositeImage($sleeves_img, Imagick::COMPOSITE_COPYOPACITY, 0, 0);
 
-
+	// Body image - Other composite options may be required
 	$body_color = new Imagick();
 	$body_color->newPseudoImage($width,$height,"canvas:". $main_color);
-
 	$body_color->compositeImage($body_img, Imagick::COMPOSITE_COPYOPACITY, 0, 0);
 
 
-
+	// Put all the peices  we made together.
 	$canvas->compositeImage($body_color, imagick::ALPHACHANNEL_COPY, 0, 0 );
-	$canvas->compositeImage($sleeve_color,imagick::ALPHACHANNEL_COPY, 0, 0 );
-	$canvas->compositeImage($ghosts,imagick::COMPOSITE_DEFAULT, 0, 0 );
+	$canvas->compositeImage($sleeve_color, imagick::ALPHACHANNEL_COPY, 0, 0 );
+	$canvas->compositeImage($ghosts, imagick::COMPOSITE_DEFAULT, 0, 0 );
 
 
-
-
+	// Output as an image. If you need to print debug info, disable this
 	header( "Content-Type: image/jpg" );
 	echo $canvas;
 	die;
 
+// so imagick can let you know when it's unhappy
 }catch(Exception $e){
 	echo 'Error: ',  $e->getMessage(), "";
 }
